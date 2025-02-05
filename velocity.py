c99f5f2d6100affd397b32be30503017cecc17f2
VarxDoesStuff <varxonstuff@gmail.com>
2025-02-05 01:46:50 +0000

    Submit

---
+++ velocity.py
@@ -0,0 +1,9 @@
+# distance and time
+distance = float(input("Enter the Distance (in meters): "))
+time = float(input("Enter the Time (in seconds): "))
+
+# velocity calc
+velocity = distance / time
+
+# answer yippee
+print(f"The velocity is {velocity:.2f} m/s.")
